b9e44cde8f9bb8ce7f7fdbcb70b50c65679cae69
James Knight <james.d.knight@live.com>
2020-06-12 01:54:51 -0400

    translator: refactor storage-format implementation into own translator
    Refactoring the existing translator to split the implementation between
    a base translator (`ConfluenceBaseTranslator`) and a new storage
    format-specific translator (`ConfluenceStorageFormatTranslator`). This
    is to help prepare for prospect changes where a new translator aims to
    support the Atlassian Document format.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/__init__.py
+++ sphinxcontrib/confluencebuilder/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-    :copyright: Copyright 2016-2020 by the contributors (see AUTHORS file).
-    :license: BSD-2-Clause, see LICENSE for details.
+:copyright: Copyright 2016-2020 Sphinx Confluence Builder Contributors (AUTHORS)
+:license: BSD-2-Clause (LICENSE)
 """
 
 from .builder import ConfluenceBuilder
@@ -11,7 +11,7 @@ from .logger import ConfluenceLogger
 from .nodes import jira
 from .nodes import jira_issue
 from .singlebuilder import SingleConfluenceBuilder
-from .translator import ConfluenceTranslator
+from .translator.storage import ConfluenceStorageFormatTranslator
 from .util import ConfluenceUtil
 from docutils import nodes
 from sphinx.util import docutils
@@ -49,9 +49,10 @@ def setup(app):
     app.require_sphinx('1.8')
     app.add_builder(ConfluenceBuilder)
     app.add_builder(SingleConfluenceBuilder)
-    app.registry.add_translator(ConfluenceBuilder.name, ConfluenceTranslator)
     app.registry.add_translator(
-        SingleConfluenceBuilder.name, ConfluenceTranslator)
+        ConfluenceBuilder.name, ConfluenceStorageFormatTranslator)
+    app.registry.add_translator(
+        SingleConfluenceBuilder.name, ConfluenceStorageFormatTranslator)
 
     # Images defined by data uri schemas can be resolved into generated images
     # after a document's post-transformation stage. After a document's doctree
